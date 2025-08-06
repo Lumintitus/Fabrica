daf8fcfed4ed07e8e055a8607b843cb38ef1e424
Lumintitus <sekarlumin@gmail.com>
2025-08-06 11:39:54 +0530

    Refactor Boy and Ourapp components for improved layout and styling; add new SVG logos for branding

--- src/Layout/Boy.tsx
+++ src/Layout/Boy.tsx
@@ -4,17 +4,55 @@ import React from 'react'
 export default function Boy() {
   return (
     <div>
-        <div className='px-9 '>
-            <div className='max-w-[19rem] max-h-full overflow-hidden relative rounded-2xl '>
-            <Image
+      <div className='flex px-9 '>
+        <div className='max-w-[19rem] max-h-full overflow-hidden relative rounded-2xl '>
+          <Image
             width={1000}
             height={1000}
-            className='w-[59rem] h-full object-cover scale-110 rounded-2xl hover:scale-125 transition-all duration-500 ease-in-out' 
-            src="/boy.jpg" 
-            alt="boy" 
-            />
-            </div>
+            className='w-[59rem] h-full object-cover scale-110 rounded-2xl hover:scale-125 transition-all duration-500 ease-in-out'
+            src="/boy.jpg"
+            alt="boy"
+          />
         </div>
+        <div className='relative left-[17rem] '>
+          <div className=' pb-[4rem] leading-[1.625rem] text-[#686868] max-w-[37rem] text-[1.375rem] tracking-tighter font-[500]'><p> <span className='pl-[6rem] text-justify font-[500] text-black tracking-tighter text-[1.375rem]'>No fluff, just results.</span> Thoughtful design and tools that make your work easier. We focus on smart design and useful features, project after project.</p></div>
+          <div className='flex gap-1'>
+            <div className=' bg-white w-[19rem] h-[7rem] rounded-2xl p-5.5 flex gap-[9rem] text-[2.813rem] tracking-tight font-[600]'><h1>50+</h1>
+              <div className='text-[#686868] text-[0.625rem] font-[600] '><h1>01</h1></div>
+            </div>
+            <div className=' bg-white w-[19rem] h-[7rem] rounded-2xl p-5.5 flex gap-[9rem] text-[2.813rem] tracking-tight font-[600]'><h1>95%</h1>
+              <div className='text-[#686868] text-[0.625rem] font-[600] '><h1>02</h1></div>
+            </div>
+          </div>
+          <div className='flex gap-1 pt-1'>
+            <div className=' bg-white w-[19rem] h-[18rem]  rounded-2xl p-8 text-[1.125rem] tracking-tight font-[500]'><h1 className='text-right'>Successful projects completed</h1>
+              <div className='text-[#686868] pt-[7.5rem] max-w-[13.1rem] text-[1rem] font-[500] '><h1>We’ve delivered 50+ projects that help companies generate real results.</h1></div>
+            </div>
+            <div className=' bg-white w-[19rem] h-[18rem]  rounded-2xl p-8  text-[1.125rem] tracking-tight font-[500]'><h1 className='max-w-[10rem] relative left-[5rem] text-right '>Customer satisfaction rate</h1>
+              <div className='flex pt-[10rem] gap-6 w-[full]'>
+                <div>  <Image
+                  src="/logo b.svg"
+                  alt="Customer satisfaction"
+                  width={1000}
+                  height={1000}
+                  className='object-cover w-[3.5rem]' /></div>
+                <div><Image
+                  src="/logoc.svg"
+                  alt="Customer satisfaction"
+                  width={1000}
+                  height={1000}
+                  className='object-cover w-[6rem]' /></div>
+                <div><Image
+                  src="/logo n.svg"
+                  alt="Customer satisfaction"
+                  width={1000}
+                  height={1000}
+                  className='object-cover w-[5rem]' /></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/Layout/Ourapp.tsx
+++ src/Layout/Ourapp.tsx
@@ -3,19 +3,18 @@ import React from 'react'
 export default function Ourapp() {
   return (
     <div>
-        <div className='flex'>
-            <div className=' px-6 w-[30%]'>
-                <div className='text-[1.25rem] font-[700] pb-[1rem]'><h1>fabrica® </h1></div>
-                <div className='text-[0.938rem] tracking-tighter text-[#686868] max-w-[13rem] font-[400]'><p>Every project we take on is designed for long-term success.</p></div>
-            </div>
-            <div className='px-6 w-[70%]'>
-                <div>
-                   
-                    <div className='tracking-tighter max-w-[48rem] text-[1.875rem] font-[500] pb-[3rem]  '><p> <span className='text-[#686868]'>Our approach is simple: </span>we focus on functionality, speed, and clarity, ensuring that every project serves a clear purpose without unnecessary complexity.</p></div>
-                    <div className='text-[#686868] max-w-[42rem] tracking-tighter text-[1.125rem] font-[449]'><p>We don’t overpromise or use flashy marketing language. We simply build well-designed, functional websites and strategies that help businesses succeed.</p></div>
-                </div>
-                </div>
+      <div className='flex'>
+        <div className=' px-6 w-[30%]'>
+          <div className='text-[1.25rem] font-[700] pb-[1rem]'><h1>fabrica® </h1></div>
+          <div className='text-[0.938rem] tracking-tighter text-[#686868] max-w-[13rem] font-[400]'><p>Every project we take on is designed for long-term success.</p></div>
         </div>
+        <div className='px-6 w-[70%]'>
+          <div>
+            <div className='tracking-tighter max-w-[48rem] text-[1.875rem] font-[500] pb-[3rem]  '><p> <span className='text-[#686868]'>Our approach is simple: </span>we focus on functionality, speed, and clarity, ensuring that every project serves a clear purpose without unnecessary complexity.</p></div>
+            <div className=' pb-[5rem] text-[#686868] max-w-[42rem] tracking-tighter text-[1.125rem] font-[449]'><p>We don’t overpromise or use flashy marketing language. We simply build well-designed, functional websites and strategies that help businesses succeed.</p></div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
